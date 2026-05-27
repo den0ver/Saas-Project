1d738a481b7025287685a1a6060ba16e34d98bd5
den0ver <dlalernen@gmail.com>
2026-05-27 21:29:22 +0200

    Initial commit

---
+++ saas-project/project/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.dashboard.urls')),
+    path('accounts/', include('apps.accounts.urls')),
+    path('companies/', include('apps.companies.urls')),
+    path('crm/', include('apps.crm.urls')),
+    path('services/', include('apps.services.urls')),
+    path('bookings', include('apps.bookings.urls')),
+    path('tasks/', include('apps.tasks.urls')),
+    path('dashboard/', include('apps.dashboard.urls')),
+    path('api/', include('apps.api.urls')),
+]
